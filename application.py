f2964d0739593963f051a085ecaefb407d09bfed
aleksbrsc <61842997+aleksbrsc@users.noreply.github.com>
2022-11-04 15:27:48 -0400

    fixed up some comments and docstrings

--- application.py
+++ application.py
@@ -1,61 +1,61 @@
 import random
 import logic
-# INPUT VALIDATION FOR Product code (checks if it isn't an int from 0-9999)
+# INPUT VALIDATION FOR Product code (breaks loop if its an integer from 0-9999)
 code=-1
 while True:
     try:
-        code = int(input("Please insert PRODUCT CODE value (0-9999): ") or -1)
+        code = int(input("Please insert PRODUCT CODE value (0-9999): "))
     except: pass
     if code >= 0 and code < 10000:
         break
     else: print("\n\u001b[31mPRODUCT CODE must be an integer from 0-9999\u001b[0m\n")
-# INPUT VALIDATION FOR Product name (checks if the input contains numbers)
+# INPUT VALIDATION FOR Product name (breaks loop if it contains alphabetical chars and length is at least 3 chars)
 name=""
 while True:
     name = input("Please insert PRODUCT NAME: ")
     if (name.isalpha()) and len(name) > 2:
-        break        
+        break
     else: print("\n\u001b[31mPRODUCT NAME must contain at least three characters and have no numbers\u001b[0m\n")
-# INPUT VALIDATION FOR Product sale price (checks if it isn't a real number greater than 0)
+# INPUT VALIDATION FOR Product sale price (breaks loop if its a real number greater than 0)
 price=-1
 while True:
     try:
-        price = float(input("Please insert PRODUCT PRICE value: ") or -1)
+        price = float(input("Please insert PRODUCT PRICE value: "))
     except: pass
     if price > 0:
         break
     else: print("\n\u001b[31mPRODUCT PRICE must be a real number greater than 0\u001b[0m\n")
 price = "{:.2f}".format(price)
-# INPUT VALIDATION FOR Product manufacturing cost (checks if it isn't a real number greater than 0)
+# INPUT VALIDATION FOR Product manufacturing cost (breaks loop if its a real number greater than 0)
 manucost=-1
 while True:
     try:
-        manucost = float(input("Please insert PRODUCT MANUFACTURING COST value: ") or -1)
+        manucost = float(input("Please insert PRODUCT MANUFACTURING COST value: "))
     except: pass
     if manucost > 0:
         break
     else:
         print("\n\u001b[31mPRODUCT MANUFACTURING COST must be a real number greater than 0\u001b[0m\n")
 manucost = "{:.2f}".format(manucost)
-# INPUT VALIDATION FOR Stock Level (checks if it isn't an integer greater than 0)
+# INPUT VALIDATION FOR Stock Level (breaks loop if its an integer greater than 0)
 stock=-1
 while True:
     try:
-        stock = int(input("Please insert PRODUCT STOCK LEVEL value: ") or -1)
+        stock = int(input("Please insert PRODUCT STOCK LEVEL value: "))
     except: pass
     if stock > 0:
         break
     else: print("\n\u001b[31mPRODUCT STOCK LEVEL must be an integer greater than 0\u001b[0m\n")
-# INPUT VALIDATION FOR  Estimated Monthly Units Manufactured (checks if it isn't an integer greater than or equal to 0)
+# INPUT VALIDATION FOR Estimated Monthly Units Manufactured (breaks loop if its an integer greater than or equal to 0)
 monthlymanu=-1
 while True:
     try:
-        monthlymanu = int(input("Please insert ESTIMATED MONTHLY UNITS MANUFACTURED value: ") or -1)
+        monthlymanu = int(input("Please insert ESTIMATED MONTHLY UNITS MANUFACTURED value: "))
     except: pass
     if monthlymanu >= 0:
         break
     else: print("\n\u001b[31mESTIMATED MONTHLY UNITS MANUFACTURED must be an integer greater than or equal to 0\u001b[0m\n")
-# PRINTS ALL INPUTS FOR USER TO SEE
+# PRINTS ALL PRODUCT INFORMATION
 print("\n~ PRODUCT INFORMATION ~\n") 
 print("Product code: " + str(code)) 
 print("Product name: " + str(name))
@@ -63,7 +63,7 @@ print("Product price: $" + str(price))
 print("Product manufacturing cost: $" + str(manucost))
 print("Product stock level: " + str(stock))
 print("Estimated monthly units manufactured: " + str(monthlymanu) + "\n")
-# OBJECT NAMED 'product' CREATED
+# ASSIGNS PRODUCT INFORMATION TO OBJECT NAMED 'product' in class Product in logic
 product = logic.Product(code,name,price,manucost,stock,monthlymanu)
 # INITIALIZING STOCK STATEMENT RELATED VARIABLES
 monthlysold=0
@@ -95,13 +95,15 @@ for i in range(1,13):
 netpnl = (float(totalsold)*float(product.price))-(float(product.monthlymanu) * float(product.manucost))
 netpnl = "{:.2f}".format(netpnl)
 # IF STATEMENT TO PRINT DOLLAR SIGN IN CORRECT LOCATION
-# prevents dollar sign before negative sign (e.g. $-100)
-if float(netpnl) >= 0:
+# prevents dollar sign before negative sign (e.g. $-100) and formats color (green=profit, zero=white, red=loss)
+if float(netpnl) > 0:
     print("\nYour Net PNL: " + "\u001b[32m$" + str(netpnl) + "\u001b[0m")
+elif float(netpnl) == 0:
+    print("\nYour Net PNL: $0")
 else:
     str(netpnl)
     print("\nYour Net PNL: \u001b[31m" + netpnl.replace("-","-$") + "\u001b[0m")
-#prints details of sales that could not be fulfilled, calculates missedprofit
+# PRINTS DETAILS OF SALES THAT COULD NOT BE FULFILLED AND CALCULATES MISSED PROFIT FOR THE YEAR
 missedprofit = float(missedsales)*float(product.price)
 missedprofit = "{:.2f}".format(missedprofit)
 if missedsales > 0:
